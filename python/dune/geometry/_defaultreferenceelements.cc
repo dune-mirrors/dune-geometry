0d89de64afd1466358207a8b5d22cfceef6969fe
Robert K <robertk@posteo.org>
2021-10-24 15:35:18 +0200

    [feature][Python] Add default reference containers to the library for dim <= 3 to reduce compile times.

---
+++ python/dune/geometry/_defaultreferenceelements.cc
@@ -0,0 +1,9 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#include <config.h>
+#define USING_DUNE_PYTHON 1
+#include <dune/python/geometry/referenceelements.hh>
+PYBIND11_MODULE( MODNAME, module )
+{
+  Dune::Python::registerReferenceElementToModule< DIM >( module );
+}
